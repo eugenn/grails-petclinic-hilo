38ebbffe63107f0592fbbe88ef0ffa2ea8e4a7c3
eugen.nekhai <Joint123>
2015-03-28 17:19:11 +0300

    sql script

---
+++ database_version.sql
@@ -0,0 +1,10 @@
+CREATE TABLE database_version
+(
+    id_owner BIGINT,
+    id_person BIGINT,
+    id_pet BIGINT,
+    id_value BIGINT,
+    id_spec BIGINT,
+    id_vet BIGINT,
+    id_visit BIGINT
+);
